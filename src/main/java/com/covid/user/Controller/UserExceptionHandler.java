47682962611f3892efd27bba585c3ddf1e457cb2
Divyam Nandanwar <ndivyam14@gmail.com>
2020-04-23 23:54:49 +0530

    Added missing import

--- src/main/java/com/covid/user/Controller/UserExceptionHandler.java
+++ src/main/java/com/covid/user/Controller/UserExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.covid.user.Controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.dao.DataIntegrityViolationException;
@@ -8,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.HttpServerErrorException.InternalServerError;
-
-import com.mongodb.MongoWriteException;
+import com.covid.Controller.UserException;
 
 @ControllerAdvice
 public class UserExceptionHandler {
